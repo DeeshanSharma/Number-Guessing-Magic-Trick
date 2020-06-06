d930ebbe98c99115f44841dd0b57307f4e3efe8c
Deeshan Sharma <princedeeshansharma@gmail.com>
2020-06-06 19:37:12 +0530

    Added some comments to better understand the program

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ Date: June 6, 2020
 Purpose: Magic trick to guess the user's number
 '''
 
+# Importing a module to speak the users number
 from win32com.client import Dispatch
 
+# Function to speak out the number of user
 def speak(line):
     speaker_number = 1
     spk = Dispatch("SAPI.SpVoice")
@@ -14,6 +16,7 @@ def speak(line):
     spk.SetVoice(vcs.Item(speaker_number))
     spk.Speak(line)
 
+# Function to display all the sets to the user and then also managing the results 
 def displayList(lis, con):
     i = 0
     while i < 5:
@@ -25,10 +28,12 @@ def displayList(lis, con):
             print("Wrong Input...")
     return check(lis, con)
 
+# Funtion to add to the sum of the users number
 def result(lis):
     Sum = lis[0]
     return Sum
 
+# Helper function for the result function
 def check(lis, con):
     res = 0
     for i, j in zip(lis, con):
@@ -36,16 +41,16 @@ def check(lis, con):
             res += result(i)
     return res
 
-print("This is a magic trick where I will guess your number...\nLet's start")
-print("\nGuess any number between 1-30")
-set1 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29]
-set2 = [2, 3, 6, 7, 10, 11, 14, 15, 18, 19, 22, 23, 26, 27, 30]
-set3 = [4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30]
-set4 = [8, 9, 10, 11, 12, 13, 14, 15, 24, 25, 26, 27, 28, 29, 30]
-set5 = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-alllist = [set1, set2, set3, set4, set5]
-con = []
-# res = 0
-
-res = displayList(alllist, con)
-speak(f"Your number is {res}")
+# Main function starts here
+if __name__ == "__main__":
+    print("This is a magic trick where I will guess your number...\nLet's start")
+    print("\nGuess any number between 1-30")
+    set1 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29]
+    set2 = [2, 3, 6, 7, 10, 11, 14, 15, 18, 19, 22, 23, 26, 27, 30]
+    set3 = [4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30]
+    set4 = [8, 9, 10, 11, 12, 13, 14, 15, 24, 25, 26, 27, 28, 29, 30]
+    set5 = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+    alllist = [set1, set2, set3, set4, set5]
+    con = []
+    res = displayList(alllist, con)
+    speak(f"Your number is {res}")
---
+++ readme.md
@@ -0,0 +1,4 @@
+This is a very simple program written in Python 3.8.2. Its purpose is to perform a magic trick where the user gusses a number between 1 an 30 and the program is designed in such a way that it is capable of guessing the number of the user by using simple mathematics.
+
+Author: Deeshan Sharma
+Date Created: June 6, 2020
